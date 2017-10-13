daa88e119779920633e216e3c54b8f4777787dd0
l630003054 <l630003054@UIC.LOCAL>
2017-10-13 15:01:45 +0800

    format

--- Lab2/task3.cpp
+++ Lab2/task3.cpp
@@ -3,6 +3,7 @@ int main(){
 	int num1, num2;
 	printf("Please input two integers\n");
 	scanf("%d%d", &num1, &num2);
+	//Operations
 	printf("%d+%d=%d\n", num1, num2, num1+num2);
 	printf("%d-%d=%d\n", num1, num2, num1-num2);
 	printf("%d*%d=%d\n", num1, num2, num1*num2);
