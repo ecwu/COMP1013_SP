a5085a80d8b5d30ac50e0aa2d9626be3d8fa2118
ecwu <bitlabwzh@gmail.com>
2017-10-28 00:54:27 +0800

    update

--- Labs/Lab4/optionaltask1.cpp
+++ Labs/Lab4/optionaltask1.cpp
@@ -9,14 +9,19 @@ int main () {
 
 	printf("Please give me an English letter: ");
 	scanf("%c", &englishLetter1);// read the first letter
-
-	if (englishLetter1 >= 97){// if the letter is the lower case letter, convert it into uppercase letter by minus 32 to it's ASCII code
+	if (!((englishLetter1 >= 65 && englishLetter1 <= 90) || (englishLetter1 >= 97 && englishLetter1 <= 122))){
+		printf("Invalid Input");
+		return -1;
+	}else if (englishLetter1 >= 97){// if the letter is the lower case letter, convert it into uppercase letter by minus 32 to it's ASCII code
 		englishLetter1 = englishLetter1 - 32;
 	}
 
 	printf("Please give me another English letter: ");
 	scanf("%*c%c", &englishLetter2);// read the second letter
-	if (englishLetter2 >= 97){// if the letter is the lower case letter, convert it into uppercase letter by minus 32 to it's ASCII code
+	if (!((englishLetter2 >= 65 && englishLetter2 <= 90) || (englishLetter2 >= 97 && englishLetter2 <= 122))){
+		printf("Invalid Input");
+		return -1;
+	}else if (englishLetter2 >= 97){// if the letter is the lower case letter, convert it into uppercase letter by minus 32 to it's ASCII code
 		englishLetter2 = englishLetter2 - 32;
 	}
 
--- Labs/Lab4/optionaltask3.cpp
+++ Labs/Lab4/optionaltask3.cpp
@@ -1,7 +1,42 @@
 # include <math.h>
 # include <stdio.h>
+
+int primeNumber(int num){
+    int a=0;
+	for(int i=2;i<num;i++){
+		if(num % i == 0){
+			a++;
+		}
+	}
+	if(a == 0){
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
 int main () {
-	int prime;
+	int status = 1, prime = 1;
+
+	for(int i = 2; i <= 100; i++){
+		for(int j = 2; j <= i / 2; j++){
+			if(i % j == 0){
+				status = 0;
+				break;
+			}
+		}
+		if(status){
+			for (int h = ((h%2 == 0)?h:h+1); h < 100; h = h + 2){
+				if(!primeNumber(h-i)){
+					printf("%d\n",h);
+				}
+			}
+		}
+		status = 1;
+		prime = 1;
+	}
+
+	/*int prime;
 		for (int even = 4; even <= 100; even = even + 2){
 			prime = 0;
 			for(int i = 2; i <= even; i++){
@@ -12,6 +47,9 @@ int main () {
 					if(j > sqrt((double)i)){
 						for(int k = j + 1; k <= even; k++){
 							for(int l = j; l <= sqrt((double)j+1); l++){
+								if(k % l==0){
+									break;
+								}
 								if (j + l == even){
 									prime = prime + 1;
 								}
@@ -24,6 +62,6 @@ int main () {
 					printf("%d ",even);
 			}
 			printf("\n");
-		}
+		}*/
 	return 0;
 }
