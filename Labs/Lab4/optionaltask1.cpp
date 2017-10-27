26b67d0b11b2e6459beb5d6493c47e4b590ddd50
ecwu <bitlabwzh@gmail.com>
2017-10-27 17:47:48 +0800

    fix error

--- Labs/Lab4/optionaltask1.cpp
+++ Labs/Lab4/optionaltask1.cpp
@@ -1,23 +1,27 @@
+/*
+	Zhenghao Wu l630003054
+	ask user to input two English Letters, and input the length between two letters(Not case sensitive)
+*/
 # include <stdio.h>
 int main () {
 	char englishLetter1, englishLetter2;
 	int difference = 0;
 
 	printf("Please give me an English letter: ");
-	scanf("%c", &englishLetter1);
+	scanf("%c", &englishLetter1);// read the first letter
 
-	if (englishLetter1 >= 97){
+	if (englishLetter1 >= 97){// if the letter is the lower case letter, convert it into uppercase letter by minus 32 to it's ASCII code
 		englishLetter1 = englishLetter1 - 32;
 	}
 
 	printf("Please give me another English letter: ");
-	scanf("%*c%c", &englishLetter2);
-	if (englishLetter2 >= 97){
+	scanf("%*c%c", &englishLetter2);// read the second letter
+	if (englishLetter2 >= 97){// if the letter is the lower case letter, convert it into uppercase letter by minus 32 to it's ASCII code
 		englishLetter2 = englishLetter2 - 32;
 	}
 
 	difference = (englishLetter1 > englishLetter2) ? (englishLetter1 - englishLetter2) : (englishLetter2 - englishLetter1);
-
+	// using ?: expression, so that the output value is always positive.
 	printf("The difference between these two letters is %d\n", difference);
 	return 0;
 }
--- Labs/Lab4/optionaltask2.cpp
+++ Labs/Lab4/optionaltask2.cpp
@@ -1,16 +1,33 @@
+/*
+	Zhenghao Wu l630003054
+	Output a patten like this:
+	#
+	++
+	###
+	++++
+	#####
+	++++++
+	#######
+	++++++
+	#####
+	++++
+	###
+	++
+	#
+*/
 # include <stdio.h>
 int main () {
-	for (int i = 0; i <= 14; i ++){
-		if (i%2 == 1){
-			for (int j = 0; j <= 7-((7-i)>0 ? (7-i) : (i-7)); j ++){
-				printf("#");
-			}
-			printf("\n");
-		}else{
-			for (int j = 0; j <= 7-((7-i)>0 ? (7-i) : (i-7)); j ++){
+	for (int i = 0; i < 14; i ++){//variable i determine the line number
+		if (i%2 == 1){//if line i is odd number line
+			for (int j = 0; j <= 6 - ((6 - i) >= 0 ? (6-i) : (i-6)); j ++){//This algorithm can calculate the number of symbols that need to be output by line number
 				printf("+");
 			}
-			printf("\n");
+			printf("\n");//print a line breaks
+		}else{//if line i is even number line
+			for (int j = 0; j <= 6 - ((6 - i) >= 0 ? (6-i) : (i-6)); j ++){//This algorithm can calculate the number of symbols that need to be output by line number
+				printf("#");
+			}
+			printf("\n");//print a line breaks
 		}
 	}
 	return 0;
