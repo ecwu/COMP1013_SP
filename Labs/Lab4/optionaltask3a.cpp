bc6edd018403466e5aa8a8a7f0b2df6e7fcccf6c
ecwu <bitlabwzh@gmail.com>
2017-10-29 11:38:07 +0800

    add comment

--- Labs/Lab4/optionaltask3a.cpp
+++ Labs/Lab4/optionaltask3a.cpp
@@ -1,51 +1,55 @@
+/*
+	Zhenghao Wu l630003054
+	see if there is an even number which cannot be the sum of two prime numbers
+*/
 # include <stdio.h>
 
-int primeMeter(int num) {
+int primeMeter(int num) { // this function can check the input parameter is a prime number or not
 	if (num <= 1) {
 		return -1;
 	}
 	int counter = 0;
-	for (int i = 2; i <= num / 2; i++) {
-		if (num % i == 0) {
+	for (int i = 2; i <= num / 2; i++) {// 
+		if (num % i == 0) {// Prime numbers can only be divided by one or itself.
 			counter = 1;
-			break;
+			break;// jump out of the loop if it can be divided with no remainder
 		}
 	}
 	if (counter) {
-		return 0;
+		return 0;// if it is not a prime number
 	}
 	else {
-		return 1;
+		return 1;// if it is a prime number
 	}
 }
 
 int main() {
 	int counter = 0, evenSum = 0;
-	for (int position = 4; position <= 100; position = position + 2) {
-		for (int i = 2; i <= 100; i++) {
+	for (int position = 4; position <= 100; position = position + 2) {// check for every even number
+		for (int i = 2; i <= 100; i++) {// generate the first prime number
 			if (primeMeter(i)) {
-				for (int j = 2; j <= 100; j++) {
+				for (int j = 2; j <= 100; j++) {// generate the second prime number
 					if (primeMeter(j) && i + j <= 100 && i + j < position + 1) {
-						if (i + j == position) {
-							counter = 1;
-							break;
+						if (i + j == position) {// check current even equal to this two prime numbers
+							counter = 1;// if the even number can be represent by two prime numbers then change the counter
+							break;// jump out of the loop
 						}
 					}
 				}
 				if (counter) {
-					break;
+					break;// jump out of the loop, and start the next number
 				}
 			}
 		}
 		if (counter) {
-			counter = 0;
+			counter = 0;//reset the counter
 		}
 		else {
-			printf("%d can not represent using two prime number\n", position);
+			printf("%d can not represent by sum of two prime number\n", position);// if there is a match
 			evenSum += position;
 		}
 	}
-	if (!evenSum) {
+	if (!evenSum) {//if there was no match
 		printf("every even number under 100 can be represent by sum of two prime number.\n");
 	}
 	return 0;
--- Labs/Lab4/optionaltask3b.cpp
+++ Labs/Lab4/optionaltask3b.cpp
@@ -1,45 +1,50 @@
+/*
+	Zhenghao Wu l630003054
+	when an even number can be represented as the sum of two prime numbers, print out the result as the following format:
+	even number = prime number 1 + prime number 2.
+*/
 # include <stdio.h>
 
-int primeMeter(int num) {
+int primeMeter(int num) { // this function can check the input parameter is a prime number or not
 	if (num <= 1) {
 		return -1;
 	}
 	int counter = 0;
-	for (int i = 2; i <= num / 2; i++) {
-		if (num % i == 0) {
+	for (int i = 2; i <= num / 2; i++) {// 
+		if (num % i == 0) {// Prime numbers can only be divided by one or itself.
 			counter = 1;
-			break;
+			break;// jump out of the loop if it can be divided with no remainder
 		}
 	}
 	if (counter) {
-		return 0;
+		return 0;// if it is not a prime number
 	}
 	else {
-		return 1;
+		return 1;// if it is a prime number
 	}
 }
 
 int main() {
 	int counter = 0;
-	for (int position = 4; position <= 100; position = position + 2) {
-		for (int i = 2; i <= 100; i++) {
+	for (int position = 4; position <= 100; position = position + 2) {// check for every even number
+		for (int i = 2; i <= 100; i++) {// generate the first prime number
 			if (primeMeter(i)) {
-				for (int j = 2; j <= 100; j++) {
+				for (int j = 2; j <= 100; j++) {// generate the second prime number
 					if (primeMeter(j) && i + j <= 100 && i + j < position + 1) {
-						if (i + j == position) {
-							printf("Even number: %d = %d + %d\n", position, i, j);
+						if (i + j == position) {// check current even equal to this two prime numbers
+							printf("%d = %d + %d\n", position, i, j); // print out the result as the required format
 							counter = 1;
-							break;
+							break;// jump out of the loop
 						}
 					}
 				}
 				if (counter) {
-					break;
+					break;// jump out of the loop, and start the next number
 				}
 			}
 		}
 		if (counter) {
-			counter = 0;
+			counter = 0;//reset the counter
 		}
 	}
 	return 0;
