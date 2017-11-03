e1bb7beb255a740e1e664bdcc6d60e126283e177
ecwu <bitlabwzh@gmail.com>
2017-11-03 15:45:45 +0800

    update optionaltask1

--- Labs/Lab5/optionaltask1.cpp
+++ Labs/Lab5/optionaltask1.cpp
@@ -15,18 +15,18 @@
 int main () {
 	int odd, lines = 0;
 	printf("please input a odd number:");
-	scanf("%d", &odd);
+	scanf("%d", &odd);// read the user input
 	if (odd % 2 == 0){
 		printf("Invalid Input\n");
 		return -1;
 	}
-	lines = (odd + 1) / 2;
+	lines = (odd + 1) / 2;// calculate the lines will output
 	for (int i = lines; i > 0; i--){
 		for (int k = i - 1; k > 0; k--){
-			printf(" ");
+			printf(" ");//print the space before the stars, numbers based on number i
 		}
 		for (int j = 0; j < (odd - 2 * i + 2); j++){
-			printf("*");
+			printf("*");//print the stars, numbers based on user's input odd
 		}
 		printf("\n");
 	}
