903264b138bcf337bf71344079b22727b51e685f
ecwu <bitlabwzh@gmail.com>
2017-11-10 15:27:17 +0800

    task3.cpp: finish basic function

--- Labs/Lab6/task3.cpp
+++ Labs/Lab6/task3.cpp
@@ -4,12 +4,37 @@
 */
 #include <stdio.h>
 int main(void){
-	char sorting[50], str[50];
-	int length = 0;
-	for (int i = 0; i < 50 && str[i-1] != -1; i++){
-		printf("please input the %d positive number: \n", i);
-		scanf("%d", &str[i]);
+	int number[100], length = 0, temp = 0, a, b, i = 0;
+	do{
+		int buffer = 0;
+		printf("please input the No.%d positive number: \n", i+1);
+		scanf("%d", &buffer);
+		if (buffer == -1){
+			break;
+		}
+		number[i] = buffer;
+		i++;
+		length ++;
 	}
-	printf("%s", str);
+	while(i < 100);
+
+	printf("Original Array:\n");
+	for (int i = 0; i < length; i++){
+		printf("%d ", number[i]);
+	}
+
+	for (a = 0; a < length - 1; a++){
+		for (b = 0; b < length - a - 1; b++){
+			if (number[b] > number[b+1]){
+				temp = number[b];
+				number[b] = number[b+1];
+				number[b+1] = temp;
+			}
+		}
+	}
+	printf("Sorted Array:\n");
+	for (int i = 0; i < length; i++){
+		printf("%d ", number[i]);
+	}
 	return 0;
 }
