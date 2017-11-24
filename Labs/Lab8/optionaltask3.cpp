ebba30f882ae51429e4e70e0eb6cbc1f2fd3caa1
ecwu <bitlabwzh@gmail.com>
2017-11-24 16:09:31 +0800

    upload op3

---
+++ Labs/Lab8/optionaltask3.cpp
@@ -0,0 +1,22 @@
+/*
+	Zhenghao Wu l630003054
+	- reading a sequence of words (in one line)
+	- call the sub‐function to sort the numbers
+	- print out the sorted words
+
+	requirement: includes two functions: main function and sort function.
+	Assume the length of the each word is less than 20
+	there are no more than 20 words
+	Library string functions can be used
+*/
+# include <stdio.h>
+# include <string.h>
+
+void sort(int original[]);
+
+int main(){
+	char input[21][21];
+	int index = 0;
+	puts("Please input a sequence of words");
+	
+}
