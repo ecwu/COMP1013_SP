6d8986729dcf0b1f45810ae35ce4083496d31eda
ecwu <bitlabwzh@gmail.com>
2017-12-01 16:53:34 +0800

    update two files

---
+++ Lab9/optionaltask1.cpp
@@ -0,0 +1,9 @@
+/*
+	Zhenghao Wu l630003054
+
+	- Write a program that includes two functions: main function, ZeroInBinary(n)
+	- main Function reads a positive integer n
+	- calls the function ZeroInBinary to compute the number of 0’s in n’s binary representation
+	
+*/
+#include <stdio.h>
---
+++ Lab9/optionaltask2.cpp
@@ -0,0 +1,8 @@
+/*
+	Zhenghao Wu l630003054
+
+	- write a program to verify all the numbers under 1000 which are the multiple of 3
+	- Use recursion.
+	
+*/
+#include <stdio.h>
