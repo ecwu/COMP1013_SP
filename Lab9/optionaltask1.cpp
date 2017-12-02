2614290771a4a216e76cee98140ab70713976f18
ecwu <bitlabwzh@gmail.com>
2017-12-02 12:36:04 +0800

    update optionaltask1

--- Lab9/optionaltask1.cpp
+++ Lab9/optionaltask1.cpp
@@ -7,3 +7,30 @@
 	
 */
 #include <stdio.h>
+
+int ZeroInBinary(int n);
+
+int main() {
+	int number = 0;
+
+	puts("Please input a positive integer:");
+	scanf("%d", &number);
+
+
+	printf("There are %d 0's in %d's binary representation", ZeroInBinary(number), number);
+	return 0;
+}
+
+int ZeroInBinary(int n){
+	if (n == 0){
+		return 1;
+	}else if (n == 1){
+		return 0;
+	}
+
+	if (n % 2 == 0){
+		return 1 + ZeroInBinary(n / 2);
+	}else if (n % 2 != 0){
+		return 0 + ZeroInBinary(n / 2);
+	}
+}
