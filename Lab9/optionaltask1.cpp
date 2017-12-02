324071e0d39c78eda303dbfcfea4d6f28c06ada9
ecwu <bitlabwzh@gmail.com>
2017-12-02 17:30:34 +0800

    finish lab9

--- Lab9/optionaltask1.cpp
+++ Lab9/optionaltask1.cpp
@@ -4,11 +4,12 @@
 	- Write a program that includes two functions: main function, ZeroInBinary(n)
 	- main Function reads a positive integer n
 	- calls the function ZeroInBinary to compute the number of 0’s in n’s binary representation
+	- Use recursion
 	
 */
 #include <stdio.h>
 
-int ZeroInBinary(int n);
+int ZeroInBinary(int n); // function prototype
 
 int main() {
 	int number = 0;
@@ -17,20 +18,22 @@ int main() {
 	scanf("%d", &number);
 
 
-	printf("There are %d 0's in %d's binary representation", ZeroInBinary(number), number);
+	printf("There are %d 0's in %d's binary representation", ZeroInBinary(number), number); // function call and print
 	return 0;
 }
 
 int ZeroInBinary(int n){
+	// base case
 	if (n == 0){
-		return 1;
+		return 1;// if the last number is zero
 	}else if (n == 1){
-		return 0;
+		return 0;// if the last number is not zero
 	}
 
 	if (n % 2 == 0){
-		return 1 + ZeroInBinary(n / 2);
+		return 1 + ZeroInBinary(n / 2); // do the recursion using Divide by 2 algorithm
 	}else if (n % 2 != 0){
-		return 0 + ZeroInBinary(n / 2);
+		return 0 + ZeroInBinary(n / 2); // recursion
 	}
+	// return the current value according to the last digit
 }
--- Lab9/optionaltask2.cpp
+++ Lab9/optionaltask2.cpp
@@ -6,3 +6,45 @@
 	
 */
 #include <stdio.h>
+#include <math.h>
+
+int verify(int n); // function prototype
+
+int main(){
+	int number = 0;
+
+	puts("Please input a integer under 1000:");
+	scanf("%d", &number);
+
+
+	printf("It takes %d times to reach 153\n", verify(number)); // function call
+	return 0;
+}
+
+int verify(int n){
+	int a = 0, b = 0, c = 0, d = 0, digits = 0, caled = 0;
+
+    for (int num = n; num > 0; num /= 10){
+    	digits++; // read how many digits in this number
+    }
+
+    switch(digits){ // calculate each digit's number
+    	case 4:
+    		a = n / 1000 % 10;
+    	case 3:
+			b = n / 100 % 10;
+		case 2:
+			c = n / 10 % 10;
+		case 1:
+			d = n / 1 % 10;
+			break;
+    }
+	
+    caled = pow(double(a), 3) + pow(double(b), 3) + pow(double(c), 3) + pow(double(d), 3); // cube each digit
+
+	if (caled == 153){
+		return 1;// base case
+	}
+
+	return 1 + verify(caled); // recursion
+}
