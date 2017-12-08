222cf9e596784d199416f6f3b00f137d2c3066f8
ecwu <bitlabwzh@gmail.com>
2017-12-09 01:56:52 +0800

    finish comment

--- Lab10/optionaltask1.cpp
+++ Lab10/optionaltask1.cpp
@@ -1,4 +1,4 @@
-	/*
+/*
 	Zhenghao Wu l630003054
 
 	- Write a function 
@@ -25,42 +25,25 @@
 #include <string.h>
 
 int occurrence1 (char *sub, char *str);
-int occurrence2 (char *sub, char *str);
+int occurrence2 (char *sub, char *str);// function prototype
 
 int main(){
 	char str1[20], str2[80];
 	scanf("%s%s", str1, str2);
-	printf("%d\n", occurrence1(str1, str2));
+	printf("%d\n", occurrence1(str1, str2));// function call
 	printf("%d\n", occurrence2(str1, str2));
 	return 0;
 }
 
-int occurrence1 (char *sub, char *str){
-	int n = 0, indicator = 0, counter = 0;
-	while (str[n] != '\0'){
-		for (int i = 0; i < strlen(sub); i++){
-			if (str[n + i] != sub[i]){
-				indicator = 1;
-				break;
-			}
-		}
-		if (!indicator){
-			counter++;
-		}
-		indicator = 0;
-		n++;
-	}
-	return counter;
-}
-int occurrence2 (char *sub, char *str){
+int occurrence1 (char *sub, char *str){ // first way: recursive
 	int indicator = 0;
 	char cmp[1] = {'\0'};
 
-	if(strcmp(str, cmp) == 0){
+	if(strcmp(str, cmp) == 0){ // base case
 		return 0;
 	}
 
-	for (int i = 0; i < strlen(sub); i++){
+	for (int i = 0; i < strlen(sub); i++){ // check next n char with str
 		if (str[i] != sub[i]){
 			indicator = 1;
 			break;
@@ -68,8 +51,27 @@ int occurrence2 (char *sub, char *str){
 	}
 
 	if(!indicator){
-		return 1 + occurrence2 (sub, (&str[0])+1);
+		return 1 + occurrence2 (sub, (&str[0])+1); // if match, start the recursion
 	}else{
-		return occurrence2 (sub, (&str[0])+1);
+		return occurrence2 (sub, (&str[0])+1); // if not match
 	}
 }
+
+int occurrence2 (char *sub, char *str){ // second way: no recursive
+	int n = 0, indicator = 0, counter = 0;
+	while (str[n] != '\0'){ // for every char index in the str
+		for (int i = 0; i < strlen(sub); i++){ // check next n char with str
+			if (str[n + i] != sub[i]){
+				indicator = 1;
+				break;
+			}
+		}
+		if (!indicator){ // if match, add 1 to counter
+			counter++;
+		}
+		indicator = 0; // reset the indicator
+		n++; // move to next char
+	}
+	return counter;
+}
+
